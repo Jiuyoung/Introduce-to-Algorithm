234f675ded63668b13a67edb2cba680185375903
jiuyoung <shuai_zhang_me@163.com>
2019-04-13 18:09:26 +0800

    合并“MergeSort”类到“FindSum”类中

--- practice1/src/test/java/cn/jiuyoung/FindSumTest.java
+++ practice1/src/test/java/cn/jiuyoung/FindSumTest.java
@@ -13,4 +13,21 @@ public class FindSumTest{
         assertTrue(FindSum.findSum(a, 226));
         assertFalse(FindSum.findSum(a, 100));
     }
+    @Test
+    public void testMerge() {
+        int[] a = {3, 432, 535, 162, 34, 64, 755, 7};
+        FindSum.MergeSort.sort(a);
+        assertTrue(isSorted(a));
+    }
+
+    
+
+    public boolean isSorted(int[] a) {
+        for (int i = 1; i < a.length; i++) {
+            if (a[i] < a[i - 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
--- practice1/src/test/java/cn/jiuyoung/SearchKthNumberTest.java
+++ practice1/src/test/java/cn/jiuyoung/SearchKthNumberTest.java
@@ -14,7 +14,7 @@ public class SearchKthNumberTest {
         int[] a = {1, 23, 56, 78, 90, 139, 26666, 335576};
         int[] b = {2, 4, 57, 88, 94, 290, 34343666};
         int[] aAndB = new int[a.length + b.length];
-        MergeSort.merge(aAndB, a, b);
+        FindSum.MergeSort.merge(aAndB, a, b);
         assertEquals(aAndB[12 - 1], SearchKthNumber.Search(a, b, 12));
     }
 }
