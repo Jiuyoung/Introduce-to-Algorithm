8a92cde70b6dd61bdb85457d136f96f79f9f1fbc
jiuyoung <shuai_zhang_me@163.com>
2019-05-14 23:40:28 +0800

    增加最短路径的动态规划求解

---
+++ practice2/src/main/java/cn/jiuyoung/ShortestPath.java
@@ -0,0 +1,57 @@
+package cn.jiuyoung;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+/**
+ * ShortestPath
+ * 两点之间的最短路径(动态规划)
+ */
+public class ShortestPath {
+
+    //记录路径长度
+    private int[] d;
+    //邻接矩阵表示图
+    private int[][] distance;
+    //保存路径
+    private int[] path;
+
+    public ShortestPath(int[][] distance) {
+        this.distance = distance;
+        d = new int[distance.length];
+        path = new int[distance.length];
+        Arrays.fill(d, Integer.MAX_VALUE);
+    }
+
+    public void execute() {
+        d[0] = 0;
+        for(int i = 0; i < distance.length; i++) {
+            for(int j = 0; j < distance.length; j++) {
+                if(distance[i][j] != 0) {
+                    int len = d[i] + distance[i][j];
+                    if(len < d[j]) {
+                        d[j] = len;
+                        path[j] = i;
+                    }
+                }
+            }
+        }
+    }
+
+    public String getResult() {
+        StringBuilder builder = new StringBuilder();
+        Stack<Integer> pStack = new Stack<>();
+        pStack.push(path.length - 1);
+        int i = path.length - 1;
+        while(path[i] != 0) {
+            pStack.push(path[i]);
+            i = path[i];
+        }
+        pStack.push(0);
+        while(!pStack.empty()) {
+            builder.append(pStack.pop()).append(" ");
+        }
+        builder.append("shortest length is ").append(d[d.length - 1]);
+        return builder.toString();
+    }
+}
